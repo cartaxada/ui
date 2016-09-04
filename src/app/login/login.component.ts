60b6f6a58d84d28847d6cdd4458f1899e851c800
Raquel Guimaraes <raquelrag@gmail.com>
2016-09-03 23:53:11 -0300

    Remove unused variable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Configuration } from '../app.configuration';
 
-const AWS = require('aws-sdk');
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 
 @Component({
