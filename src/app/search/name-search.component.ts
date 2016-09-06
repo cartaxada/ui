5688f5e4536787a9e0e7ec0f35a0f65156a117a3
Raquel Guimaraes <raquelrag@gmail.com>
2016-09-06 19:58:45 -0300

    UI improvements on search by name

--- src/app/search/name-search.component.ts
+++ src/app/search/name-search.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 import { FamilyMember } from '../family-member/fm.model';
 import { DynamoService } from '../service/dynamo.service';
+import { Configuration } from '../app.configuration';
 
 @Component({
   template: require('./name-search.template.html')
 })
 export class NameSearchComponent {
+
+  pictureBucket: string = Configuration.pictureBucket;
   searchQuery: string;
   searchResult: FamilyMember[];
 
   constructor(private dynamoService: DynamoService) {}
 
   search() {
-    this.dynamoService.searchByName(this.searchQuery)
-        .then((res) => {
-          this.searchResult = res;
-        }).catch((err) => {
-          console.log(JSON.stringify(err));
-        });
-  }
+    if (this.searchQuery && this.searchQuery.trim()) {
+      this.dynamoService.searchByName(this.searchQuery.trim())
+          .then((res) => {
+            this.searchResult = res;
+          }).catch((err) => {
+            console.log(JSON.stringify(err));
+          });
+    }
+}
 
 }
